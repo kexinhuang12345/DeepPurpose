e4eb5e7d23901f8e9bb812dd3db6fdcf1b9e6f3c
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-04-03 20:12:53 -0400

    test py file

---
+++ test_deepDTA.py
@@ -0,0 +1,27 @@
+import DeepPurpose.models as models
+from DeepPurpose.utils import *
+from DeepPurpose.dataset import *
+import os
+
+X_drug, X_target, y = load_process_DAVIS('./data/', binary=True)
+
+drug_encoding = 'CNN'
+target_encoding = 'CNN'
+train, val, test = data_process(X_drug, X_target, y, 
+                                drug_encoding, target_encoding, 
+                                split_method='random',frac=[0.7,0.1,0.2])
+
+config = generate_config(drug_encoding = drug_encoding, 
+                         target_encoding = target_encoding, 
+                         cls_hidden_dims = [1024,1024,512], 
+                         train_epoch = 100, 
+                         LR = 0.001, 
+                         batch_size = 256,
+                         cnn_drug_filters = [32,64,96],
+                         cnn_target_filters = [32,64,96],
+                         cnn_drug_kernels = [4,6,8],
+                         cnn_target_kernels = [4,8,12]
+                        )
+model = models.model_initialize(**config)
+model.train(train, val, test)
+model.save_model('./model')
