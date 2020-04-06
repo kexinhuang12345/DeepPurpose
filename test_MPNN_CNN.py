e41a37a8fa4d7352031eb021571a6e6dae81af2f
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-04-06 14:26:30 -0400

    hpc
    Former-commit-id: 4e8510328a75d882529377fc700457bfe2540d5f

--- test_MPNN_CNN.py
+++ test_MPNN_CNN.py
@@ -7,7 +7,7 @@ X_drug, X_target, y  = process_BindingDB('./data/BindingDB_All.tsv',
                                          binary = False, 
                                          convert_to_log = True)
 
-drug_encoding = 'Morgan'
+drug_encoding = 'MPNN'
 target_encoding = 'CNN'
 train, val, test = data_process(X_drug, X_target, y, 
                                 drug_encoding, target_encoding, 
@@ -18,12 +18,13 @@ config = generate_config(drug_encoding = drug_encoding,
                          cls_hidden_dims = [1024,1024,512], 
                          train_epoch = 1, 
                          LR = 0.001, 
-                         batch_size = 256,
-                         cnn_drug_filters = [32,64,96],
+                         batch_size = 16,
                          cnn_target_filters = [32,64,96],
-                         cnn_drug_kernels = [4,6,8],
-                         cnn_target_kernels = [4,8,12]
+                         cnn_target_kernels = [4,8,12],
+                         hidden_dim_drug = 128,
+                         mpnn_hidden_size = 128,
+                         mpnn_depth = 3
                         )
 model = models.model_initialize(**config)
 model.train(train, val, test)
-model.save_model('./model_Morgan_CNN')
+model.save_model('./model_MPNN_CNN')
