ac124b40f3f87c1dae34faea01bfed1fc8cda538
futianfan <futianfan@gmail.com>
2020-04-09 00:23:31 -0400

    demo

---
+++ DEMO/GraphDTA.py
@@ -0,0 +1,25 @@
+
+import os
+import sys 
+#sys.path.append("../../")
+#path = "/Users/futianfan/Downloads/spring2020/"
+#os.chdir("../..")
+
+os.chdir("../..")
+
+
+import DeepPurpose.models as models
+#from DeepPurpose.utils import *
+#from DeepPurpose.dataset import *
+
+
+'''
+import DeepPurpose.models as models
+from DeepPurpose.utils import *
+from DeepPurpose.dataset import *
+''' 
+
+
+
+
+
