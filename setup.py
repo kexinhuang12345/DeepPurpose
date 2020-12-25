a53e6ca1daaab68164c424b9059e8e102fceffb4
Kexin Huang <kexinhuang@hsph.harvard.edu>
2020-12-24 17:17:01 -0800

    prep for pip install

--- setup.py
+++ setup.py
@@ -1,21 +1,30 @@
-import setuptools
+from setuptools import setup
+from os import path
+from io import open
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+this_directory = path.abspath(path.dirname(__file__))
 
-setuptools.setup(
+def readme():
+    with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
     name="DeepPurpose", 
-    version="0.0.1",
+    version="0.0.3",
     author="Kexin Huang, Tianfan Fu",
+    license="BSD-3-Clause",
     author_email="kexinhuang@hsph.harvard.edu",
-    description="a Deep Learning Based Drug Repurposing and Virtual Screening Toolkit",
-    long_description=long_description,
+    description="a Deep Learning Based Toolkit for Drug Discovery",
+    long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://github.com/kexinhuang12345/DeepPurpose",
-    packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Creative Commons Zero v1.0 Universal",
+        "License :: OSI Approved :: BSD-3-Clause License",
         "Operating System :: OS Independent",
     ],
 )
