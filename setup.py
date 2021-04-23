fa3ee21ced0ae9bbf19453079b9a819ba8b121ba
Kexin Huang <kexinhuang@hsph.harvard.edu>
2021-04-23 15:02:48 -0700

    0.1.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     name="DeepPurpose", 
     packages = ['DeepPurpose'],
     package_data={'DeepPurpose': ['ESPF/*']},
-    version="0.0.9",
+    version="0.1.0",
     author="Kexin Huang, Tianfan Fu",
     license="BSD-3-Clause",
     author_email="kexinhuang@hsph.harvard.edu",
